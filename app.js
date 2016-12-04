0c80b561d80fa5209f7fa468cc106a61a2645eee
Michael Zhong <mzhong21@gmail.com>
2016-12-04 09:04:42 -0800

    listen to right port

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express')
 var app = express()
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html')
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'))
 })
